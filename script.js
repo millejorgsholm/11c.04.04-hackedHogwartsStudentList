84a044609916deef528f472363a1a047e24383f5
Mille Jørgsholm Bierrings <mill1197@stud.kea.dk>
2021-02-23 09:41:51 +0100

    gender added

--- script.js
+++ script.js
@@ -215,7 +215,7 @@ function displayStudent(student) {
   //   student.middleName;
   clone.querySelector("[data-field=lastname]").textContent = student.lastName;
   // clone.querySelector("[data-field=nickname]").textContent = student.nickName;
-  // clone.querySelector("[data-field=gender]").textContent = student.gender;
+  clone.querySelector("[data-field=gender]").textContent = student.gender;
   clone.querySelector("[data-field=house]").textContent = student.house;
   clone.querySelector("[data-field=image] img").src = `images/${
     student.lastName
